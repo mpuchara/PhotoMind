de932df50102c6a8e5ca1b6ebe7202cc2b2a9ad3
mpuchara <mpuchara@gmail.com>
2026-09-07 23:17:32 +0200

    Add photo model

---
+++ app/src/main/java/com/photomind/app/PhotoItem.kt
@@ -0,0 +1,13 @@
+package com.photomind.app
+
+data class PhotoItem(
+    val mediaId: Long,
+    val uri: String,
+    val displayName: String,
+    val bucket: String,
+    val dateTaken: Long,
+    val dateModified: Long,
+    val labels: String,
+    val ocr: String,
+    var userTags: String
+)
